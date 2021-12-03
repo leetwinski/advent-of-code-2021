b220320cdbcdacdfd73fa661c6bd79ef4a0ab203
Victor Litvintsev <leetwinski@gmail.com>
2021-12-03 18:41:23 +0300

    update min-max

--- src/advent_2021/task3.clj
+++ src/advent_2021/task3.clj
@@ -1,34 +1,31 @@
 (ns advent-2021.task3
   (:require [clojure.string :refer [split-lines join]]))
 
-(defn min-max [data]
-  (let [{ones \1 zeros \0 :or {ones 0 zeros 0}} (frequencies data)]
-    (zipmap [:min :max]
-            (if (< ones zeros) "10" "01"))))
+(def min-max #(sort-by (frequencies %) "01"))
 
 (def map-column #(partial apply map (comp % vector)))
 
 ;; part 1 solution
 (def solve1
   (comp (map-column join)
-        (map-column (comp vals min-max))))
+        (map-column min-max)))
 
 ;; part 2 solution
 (defn solve2 [data]
-  (letfn [(narrow [min-or-max data]
+  (letfn [(narrow [min-max-component data]
             (if (= 1 (count data))
               (first data)
               (let [first-char (->> data
                                     (map first)
                                     min-max
-                                    min-or-max)]
+                                    min-max-component)]
                 (->> data
                      (filter #(= first-char (first %)))
                      (map #(subs % 1))
-                     (narrow min-or-max)
+                     (narrow min-max-component)
                      (str first-char)))))]
-    [(narrow :min data)
-     (narrow :max data)]))
+    [(narrow first data)
+     (narrow second data)]))
 
 (defmacro execute []
   (let [data (split-lines (slurp "./resources/data3.txt"))
