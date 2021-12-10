8d6a5040407b81c7014886b33e0533255d02423a
Victor Litvintsev <leetwinski@gmail.com>
2021-12-10 19:27:02 +0300

    cleanup

--- src/advent_2021/task10.clj
+++ src/advent_2021/task10.clj
@@ -4,8 +4,7 @@
 (let [opening "[{(<"
       closing "]})>"]
   (def opening? (set opening))
-  (def closing? (set closing))
-  (def opening-by-closing (zipmap closing opening)))
+  (def closing->opening (zipmap closing opening)))
 
 (def err-price {\) 3 \] 57 \} 1197 \> 25137})
 
@@ -22,7 +21,7 @@
 (defn process [line]
   (reduce (fn [{:keys [stack]} x]
             (cond (opening? x) {:stack (conj stack x)}
-                  (= (opening-by-closing x) (peek stack)) {:stack (pop stack)}
+                  (= (closing->opening x) (peek stack)) {:stack (pop stack)}
                   :else (reduced {:err x :stack stack})))
           {:stack []}
           line))
